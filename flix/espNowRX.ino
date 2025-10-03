f8444366bc70e83334707e221298af505e2bb0a9
mituritsyn <mikhail-turicyn@yandex.ru>
2025-10-03 23:49:45 +0200

    fix espnow rx

--- flix/espNowRX.ino
+++ flix/espNowRX.ino
@@ -3,6 +3,7 @@
 
 extern float controlRoll, controlPitch, controlThrottle, controlYaw;
 extern uint16_t channels[16];
+extern double controlTime;
 // Структура для получаемых данных (должна совпадать с отправителем)
 typedef struct {
   int8_t sticks[4];
@@ -26,16 +27,15 @@ void onDataRecv(const esp_now_recv_info_t *recv_info, const uint8_t *incomingDat
   newMessageReceived = true;
 }
 void readEspNowRX() {
-  if (newMessageReceived){
+  if (newMessageReceived) {
     newMessageReceived = false;
 
     // Обработка полученных данных
     controlRoll = rxPacket.sticks[1] / 127.0f;
     controlPitch = rxPacket.sticks[0] / 127.0f;
-    controlThrottle = rxPacket.sticks[3] / 127.0f;
+    // Преобразуем [-127;127] в [0;1]
+    controlThrottle = (rxPacket.sticks[3] + 127) / 254.0f;
     controlYaw = rxPacket.sticks[2] / 127.0f;
-    // Serial.printf("sticks: %d, %d, %d, %d\n", rxPacket.sticks[0], rxPacket.sticks[1], rxPacket.sticks[2], rxPacket.sticks[3]);
-    // Serial.printf("Received: R: %.2f, P: %.2f, T: %.2f, Y: %.2f, M: %d\n", controlRoll, controlPitch, controlThrottle, controlYaw, rxPacket.button);
-    // controlMode = rxPacket.button;
+    controlTime = t;
   }
 }
